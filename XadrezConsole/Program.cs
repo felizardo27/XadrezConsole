70ee5cef7dbc41135ba29fcf457b8e9e70c44dff
Joao Pedro Felizardo <jp.felizardo27@gmail.com>
2022-04-26 10:34:21 -0300

    Implementando as restricoes de movimentos

--- XadrezConsole/Program.cs
+++ XadrezConsole/Program.cs
@@ -15,27 +15,41 @@ namespace XadrezConsole
 
                 while (!partida.Terminada)
                 {
-                    Console.Clear();
-                    Tela.ImprimirTabuleiro(partida.Tab);
+                    try
+                    {
+                        Console.Clear();
+                        Tela.ImprimirTabuleiro(partida.Tab);
+                        Console.WriteLine();
+                        Console.WriteLine($"Turno: {partida.Turno}");
+                        Console.WriteLine($"Aguardando jogada: {partida.JogadorAtual}");
 
-                    Console.WriteLine();
-                    Console.Write("Origem: ");
-                    Posicao origem = Tela.LerPosicaoXadrez().ToPosicao();
 
-                    // a partir da posição de origem que o usuário digitou,
-                    // vai acessar essa peça de origem que está na posição de origem
-                    // vai pegar os MovimentosPossiveis dessa peça e guardar nessa matriz Bool
-                    bool[,] posicoesPossiveis = partida.Tab.Peca(origem).MovimentosPossiveis();
+                        Console.WriteLine();
+                        Console.Write("Origem: ");
+                        Posicao origem = Tela.LerPosicaoXadrez().ToPosicao();
+                        partida.ValidarPosicaoDeOrigem(origem);
 
-                    // limpar a tela e mostrar no tabuleiro os movimentos possíveis
-                    Console.Clear();
-                    Tela.ImprimirTabuleiro(partida.Tab, posicoesPossiveis);
+                        // a partir da posição de origem que o usuário digitou,
+                        // vai acessar essa peça de origem que está na posição de origem
+                        // vai pegar os MovimentosPossiveis dessa peça e guardar nessa matriz Bool
+                        bool[,] posicoesPossiveis = partida.Tab.Peca(origem).MovimentosPossiveis();
 
-                    Console.WriteLine();
-                    Console.Write("Destino: ");
-                    Posicao destino = Tela.LerPosicaoXadrez().ToPosicao();
-                
-                    partida.ExecutaMovimento(origem, destino);
+                        // limpar a tela e mostrar no tabuleiro os movimentos possíveis
+                        Console.Clear();
+                        Tela.ImprimirTabuleiro(partida.Tab, posicoesPossiveis);
+
+                        Console.WriteLine();
+                        Console.Write("Destino: ");
+                        Posicao destino = Tela.LerPosicaoXadrez().ToPosicao();
+                        partida.ValidarPosicaoDeDestino(origem, destino);
+
+                        partida.RealizaJogada(origem, destino);
+                    }
+                    catch (TabuleiroException e)
+                    {
+                        Console.WriteLine(e.Message);
+                        Console.ReadLine();
+                    }
                 }
 
                 
